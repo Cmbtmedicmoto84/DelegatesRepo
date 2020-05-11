e2aa3716c58d75c05aebb3e69a4822ebf9778e4e
Steven Morimoto <smorimoto2284@gmail.com>
2020-05-11 11:15:16 -0500

    init

---
+++ DelegatesDemo/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    //Calculate is the name of the delegate
+    //Calculation is the parameter that represents the delegate in PrintResult()
+    public class Calculator
+    {
+        double sum;
+        public delegate double Calculate(double firstNumber, double secondNumber);
+        public delegate void AssignSum(double firstNumber, double secondNumber);
+
+        public double AddTwoNumbers(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public double MultiplyTwoNumbers(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public void PrintSum(double firstNumber, double secondNumber)
+        {
+            sum = firstNumber + secondNumber;
+        }
+        public double PrintResult(double firstNumber, double secondNumber, Calculate Calculation)
+        {
+            return Calculation(firstNumber, secondNumber);
+            //Console.WriteLine(result);
+        }
+
+        public string GetName()
+        {
+            Console.WriteLine("enter your name");
+            var name = Console.ReadLine();
+            return name;
+        }
+    }
+}
---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Printer printer = new Printer();
+            //printer.Execute();
+            printer.TestDelegate();
+
+            //*****************************************************************************************
+            
+            Calculator calculator = new Calculator();
+            NewCalculator newCalculator = new NewCalculator();
+
+            //calculator.PrintResult(10, 20, newCalculator.SetSum);
+
+            //calculator.PrintResult(10, 20, calculator.MultiplyTwoNumbers);
+
+            //*****************************************************************************************
+
+            List<int> grades = new List<int>() { 100, 85, 97, 61, 55, 89 };
+            IEnumerable<int> result = GradeChecker.CheckGrades(grades, GradeChecker.GreaterThanSeventy);
+            foreach (int grade in result)
+            {
+                Console.WriteLine(grade);
+            }
+
+            //*****************************************************************************************
+
+            MovieLibrary movieLibrary = new MovieLibrary();
+            List<Movie> movies = movieLibrary.GetMovies();
+
+            List<Movie> hourAndAHalfMovieRunTime = movies.FindAll(m => m.runTime > 90);
+            foreach (var movie in hourAndAHalfMovieRunTime)
+            {
+                Console.WriteLine(movie.title);
+            }
+
+            //*****************************************************************************************
+
+            //The Departed, Inception, The Dark Knight
+
+            var ninetyMinuteMovies = movies.Where(m => m.runTime >= 90); //The Departed, The Dark Knight
+
+            var moviesSorted = ninetyMinuteMovies.OrderBy(m => m.title); //The Dark Knight, The Departed
+
+            var movieTitles = moviesSorted.Select(m => m.title); //"The Dark Knight", "The Departed"
+            
+        }
+
+
+        //m => m.runTime > 90
+        public bool CheckEachMovie(Movie m)
+        {
+            if(m.runTime > 90)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
